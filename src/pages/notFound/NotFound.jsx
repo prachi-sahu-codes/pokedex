73d7692cdee8ed2210b2cd964069a316979ac1c4
prachi-sahu-codes <prachisahu998@gmail.com>
2023-12-23 20:15:53 +0530

    style: spacing

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -12,10 +12,10 @@ const NotFound = () => {
     <div className="w-screen h-screen">
       <div className="absolute top-2/4 left-2/4 -translate-x-1/2 -translate-y-2/4 text-center">
         <img src={notfound} alt="Not found" className="w-52 sm:w-96 m-auto mb-8" />
-        <h1 className="w-60 sm:w-full font-semibold text-lg sm:text-2xl p-2 text-black">
+        <h1 className="w-60 sm:w-full font-semibold text-lg sm:text-xl p-2 text-black">
           Looks like something is missing!!
         </h1>
-        <p className="text-black text-xs sm:text-base">Go back to explore page</p>
+        <p className="text-gray-500 text-xs sm:text-sm mb-3">Go back to explore page</p>
         <Button action={clickHandler} text="Explore" />
       </div>
     </div>
