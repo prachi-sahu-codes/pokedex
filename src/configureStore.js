65b2911fb58ad1b43b210ddc5f111893be11b94d
prachi-sahu-codes <prachisahu998@gmail.com>
2023-12-23 20:14:44 +0530

    feat: redux setup

---
+++ src/configureStore.js
@@ -0,0 +1,12 @@
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from "redux";
+import { thunk } from "redux-thunk";
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
