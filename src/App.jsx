e6630fc819432744aedcfe8cd6b7b030c01afb24
prachi-sahu-codes <prachisahu998@gmail.com>
2023-12-23 20:22:01 +0530

    feat: fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router";
+import { useDispatch } from "react-redux";
+import "./App.css";
 import { Home, NotFound } from "./pages";
+import {
+  getAllPokemons,
+  getLimitedPokemons,
+} from "./store/pokemonStore/action";
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getAllPokemons());
+    dispatch(getLimitedPokemons());
+  }, []);
+
   return (
     <div>
       <Routes>
---
+++ src/store/pokemonStore/action.js
@@ -0,0 +1,36 @@
+import { baseURL } from "../../constants/urls";
+import axios from "axios";
+
+const offset = 0;
+
+const getAllPokemons =
+  (limit = 40) =>
+  async (dispatch) => {
+    const response = await axios.get(
+      `${baseURL}pokemon?limit=${limit}&offset=${offset}`
+    );
+
+    const allPromises = response?.data?.results?.map(
+      async ({ url }) => await axios.get(url)
+    );
+
+    const data = await axios.all(allPromises);
+
+    const extractArray = data.map((obj) => obj.data);
+    console.log(extractArray);
+  };
+
+const getLimitedPokemons = async () => {
+  const response = await axios.get(`${baseURL}pokemon?limit=100000&offset=0`);
+
+  const allPromises = response?.data?.results?.map(
+    async ({ url }) => await axios.get(url)
+  );
+
+  const data = await axios.all(allPromises);
+
+  const extractArray = data.map((obj) => obj.data);
+  console.log(extractArray);
+};
+
+export { getAllPokemons, getLimitedPokemons };
