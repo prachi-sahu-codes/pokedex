0e33f0c039d06e129e490dbfd50f1c19f603352d
prachi-sahu-codes <prachisahu998@gmail.com>
2023-12-23 20:16:30 +0530

    style: background color changed

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Button = ({ text, action }) => {
+const Button = ({ text, action, style }) => {
   return (
     <button
-      className="bg-orange-600 hover:bg-orange-700 text-base sm:text-lg text-white py-1 px-5 m-4 min-w-10 rounded-full shadow-md active:shadow-none"
+      className={`text-sm border border-orange-600 text-orange-600 hover:bg-orange-600 hover:text-white py-1 px-5 m-4 min-w-10 rounded-full shadow-md active:shadow-none pb-[5px] ${style}`}
       onClick={action}
     >
       {text}
