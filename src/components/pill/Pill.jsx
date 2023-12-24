8ff0298a2a0ad527b539e63fcd0222749f245d12
prachi-sahu-codes <prachisahu998@gmail.com>
2023-12-25 03:06:41 +0530

    feat: pill

---
+++ src/components/pill/Pill.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pill = ({ text }) => {
+  return <div className="bg-white p-1 px-3 rounded-full bg-opacity-30 text-white w-fit text-sm capitalize">{text}</div>;
+};
+
+export default Pill;
