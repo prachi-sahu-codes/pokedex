968652c28a83a47ad5f2c5a7e3e36925a5670ae2
prachi-sahu-codes <prachisahu998@gmail.com>
2023-12-25 03:04:47 +0530

    style: screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm380: "380px",
+      sm: "640px",
+      md700: "700px",
+      md: "768px",
+      lg: "1024px",
+      lg1150: "1150px",
+      xl: "1280px",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
